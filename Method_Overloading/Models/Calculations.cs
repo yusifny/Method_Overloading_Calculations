29606ed83d6dec788f14c435a9caeb0a0344ac2d
Yusif Nazarbayov (CodeAcademy) <99797435+yusifny@users.noreply.github.com>
2022-04-16 11:35:20 +0400

    feat: "project added"

---
+++ Method_Overloading/Models/Calculations.cs
@@ -0,0 +1,89 @@
+﻿using System;
+namespace Method_Overloading.Models
+{
+	public class Calculations
+	{
+        #region Sum
+            public int Sum(int a, int b)
+            {
+                return a + b;
+            }
+            public int Sum(int a, int b, int c)
+            {
+                return a + b + c;
+            }
+            public int Sum(int a, int b, int c, int d)
+            {
+                return a + b + c + d;
+            }
+
+            public int Sum(int a, int b, int c, int d, int e)
+            {
+                return a + b + c + d + e;
+            }
+        #endregion
+
+        #region Minus
+            public int Minus(int a, int b)
+            {
+                return a - b;
+            }
+            public int Minus(int a, int b, int c)
+            {
+                return a - b - c;
+            }
+            public int Minus(int a, int b, int c, int d)
+            {
+                return a - b - c - d;
+            }
+            public int Minus( int a, int b, int c, int d, int e)
+            {
+                return a - b - c - d - e;
+            }
+        #endregion
+
+        #region Multiply
+
+            public int Multiply(int a, int b)
+            {
+                return a * b;
+            }
+            public int Multiply(int a, int b, int c)
+            {
+                return a * b * c;
+            }
+            public int Multiply(int a, int b, int c, int d)
+            {
+                return a * b * c * d;
+            }
+            public int Multiply(int a, int b, int c, int d, int e)
+            {
+                return a * b * c * d * e;
+            }
+
+        #endregion
+
+        #region Divide
+
+            public double Divide(int a, int b)
+            {
+                return (double) a / b;
+            }
+            public double Divide(int a, int b, int c)
+            {
+                return (double) a / b / c;
+            }
+            public double Divide(int a, int b, int c, int d)
+            {
+                return (double) a / b / c / d;
+            }
+            public double Divide(int a, int b, int c, int d, int e)
+            {
+                return (double) a / b / c / d / e;
+            }
+
+        #endregion
+
+    }
+}
+
---
+++ Method_Overloading/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Method_Overloading.Models;
+
+namespace Method_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculations calc = new Calculations();
+
+            //Console.WriteLine(calc.Sum());
+
+            //Console.WriteLine(calc.Minus());
+
+            //Console.WriteLine(calc.Multiply());
+
+            //Console.WriteLine(calc.Divide(2, 4));
+
+        }
+    }
+}
+
